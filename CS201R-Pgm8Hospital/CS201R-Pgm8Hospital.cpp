abcc372434a3275bddd9639747a8a2a88c273299
Joan Gladbach <jjgr29@umsystem.edu>
2024-11-06 13:16:17 -0600

    Add project files.

---
+++ CS201R-Pgm8Hospital/CS201R-Pgm8Hospital.cpp
@@ -0,0 +1,10 @@
+// CS201R-Pgm8Hospital.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Sadly, there is no starter code for this assignment\n";
+}
+ 
